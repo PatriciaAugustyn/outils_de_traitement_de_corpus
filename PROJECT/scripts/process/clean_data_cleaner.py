765460c7593516e4575daff0413ccbf685b28424
PatriciaAugustyn <patricia.augustyn@sorbonne-nouvelle.fr>
2024-05-18 16:18:27 +0200

    fix: ajout des docstring sur clean_data_cleaner.ipynb

--- PROJECT/scripts/process/clean_data_cleaner.py
+++ PROJECT/scripts/process/clean_data_cleaner.py
@@ -15,16 +15,27 @@ import glob
 
 
 def clean_to_text(path_xml, path_clean):
-    """Cette fonction permet de lire les fichiers XML dont le chemin est donné en
-    argument, et qui retourne le texte et les métadonnées des items du flux RSS."""
+    """
+    Cette fonction permet de Lire les fichiers XML depuis un chemin donné, extrait le texte et les métadonnées des balises spécifiées, et enregistre
+    le contenu nettoyé dans des fichiers .txt.
 
+    Paramètres:
+    path_xml (str): Le chemin global pour les fichiers XML à lire.
+    path_clean (str): Le chemin du dossier où les fichiers texte nettoyés seront enregistrés.
+
+    Retourne:
+    On retourne le texte et les métadonnées des items du flux RSS.
+
+    Ainsi, on recherche les fichiers XML dans le chemin spécifié, extrait les contenus des balises <link>, <title>, <description> et <content>, et
+    écrit ces contenus dans des fichiers texte dans le dossier spécifié.
+    """
     # Utilisation de glob pour obtenir une liste de tous les fichiers XML dans le répertoire spécifié
     fichiers_xml = glob.glob(path_xml)
 
     for fichier_xml in fichiers_xml:
         with open(fichier_xml, "r", encoding="utf-8") as fichier:
             xml = fichier.read()
-            # On veut cibler notre regex sur ce qui est seulement à l'intérieur des balises <link></link>, <title></title>, <description></description> et <content></content> : donc ()
+            # On veut cibler notre regex sur ce qui est seulement à l'intérieur des balises <link></link>, <title></title>, <description><description> et <content></content> : donc ()
 
             regex_titre = re.findall(r"<title>(.*)</title>", xml)
             regex_description = re.findall(r"<description>(.*)</description>", xml)
